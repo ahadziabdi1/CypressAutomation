9449fa612daf50a08348d0eae5a007b7ff4bf5b6
ahadziabdi1 <ahadziabdi1@etf.unsa.ba>
2024-10-12 10:53:18 +0200

    Implementing the sum of products funcionality with cypess-js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 6000,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: 'cypress/integration/examples/*.js'
+    specPattern: "cypress/integration/examples/*.js",
   },
 });
--- cypress/integration/examples/TestFramework.js
+++ cypress/integration/examples/TestFramework.js
@@ -25,5 +25,37 @@ describe("My Second Test", () => {
       cy.selectProduct(element);
     });
     productPage.getCheckOutButton().click();
+    var sum = 0;
+    cy.get("tr td:nth-child(4) strong")
+      .each(($el, index, $list) => {
+        //cy.log($el.text());
+        const amount = $el.text();
+        var resulat = amount.split(" ");
+        resulat = resulat[1].trim();
+        //cy.log(resulat)
+        sum = Number(sum) + Number(resulat);
+      })
+      .then(function () {
+        cy.log(sum);
+      });
+    cy.get("h3 strong").then(function (element) {
+      const amount = element.text();
+      var resulat = amount.split(" ");
+      var total = resulat[1].trim();
+      expect(Number(total)).to.equal(sum);
+    });
+    cy.contains("Checkout").click();
+    cy.get("#country").type("India");
+    cy.get(".suggestions > ul > li > a").click();
+    cy.get("#checkbox2").click({ force: true });
+    cy.get("input[type='submit']").click();
+    //cy.get(".alert").should(
+    //  "have.text",
+    //  "Success! Thank you! Your order will be delivered in next few weeks :-)."
+    //);
+    cy.get(".alert").then(function (element) {
+      const actualText = element.text();
+      expect(actualText.includes("Success")).to.be.true;
+    });
   });
 });
