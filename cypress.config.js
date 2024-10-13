819e14062240661bd78afa2a764acd73662f5fde
ahadziabdi1 <ahadziabdi1@etf.unsa.ba>
2024-10-13 19:17:07 +0200

    Exploring cypress dashboard and its feature for framework development

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'ermsq2',
   defaultCommandTimeout: 6000,
 
   reporter: "cypress-mochawesome-reporter",
 
+  projectId: "ermsq2",
+  
   env: {
     url: "https://rahulshettyacademy.com",
   },
